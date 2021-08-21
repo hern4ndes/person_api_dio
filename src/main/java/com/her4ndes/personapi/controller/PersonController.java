c249bff0b40214125be5b59f0a63dac367ae83f9
Hernandes Erick <erickhernandes4@gmail.com>
2021-08-21 12:00:05 -0300

    Concluded endpoint to create a person

--- src/main/java/com/her4ndes/personapi/controller/PersonController.java
+++ src/main/java/com/her4ndes/personapi/controller/PersonController.java
@@ -1,6 +1,14 @@
 package com.her4ndes.personapi.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import com.her4ndes.personapi.dto.MessageResponseDTO;
+import com.her4ndes.personapi.entity.Person;
+import com.her4ndes.personapi.repository.PersonRepository;
+
+
+import com.her4ndes.personapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +16,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/people")
 public class PersonController {
 
-    @GetMapping
-    public String getBook(){
-        return "API test";
+    private PersonRepository personRepository;
+
+
+    @Autowired
+    public PersonController(PersonRepository personReposiry){
+        this.personRepository = personReposiry;
+    }
+    @PostMapping
+    public MessageResponseDTO creatPerson(@RequestBody Person person){
+        Person savedPerson = personRepository.save(person);
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID"+savedPerson.getId())
+                .build();
+
     }
 }
