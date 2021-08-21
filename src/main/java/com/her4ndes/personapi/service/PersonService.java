ec9dcc6981ac2e23033a744ccca7a2bb9cd636a9
Hernandes Erick <erickhernandes4@gmail.com>
2021-08-21 12:30:48 -0300

    Created person service

---
+++ src/main/java/com/her4ndes/personapi/service/PersonService.java
@@ -0,0 +1,30 @@
+package com.her4ndes.personapi.service;
+
+import com.her4ndes.personapi.dto.MessageResponseDTO;
+import com.her4ndes.personapi.entity.Person;
+import com.her4ndes.personapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Service
+public class PersonService {
+
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    @PostMapping
+    public MessageResponseDTO creatPerson( Person person){
+        Person savedPerson = personRepository.save(person);
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID"+savedPerson.getId())
+                .build();
+
+    }
+}
