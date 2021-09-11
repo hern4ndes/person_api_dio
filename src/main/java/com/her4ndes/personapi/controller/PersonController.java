67bdbeaf1c53ba39b81faaafb30ef500cb84a7d2
Hernandes Erick <erickhernandes4@gmail.com>
2021-09-10 21:49:09 -0300

    Implementation of delet by Id endpoint

--- src/main/java/com/her4ndes/personapi/controller/PersonController.java
+++ src/main/java/com/her4ndes/personapi/controller/PersonController.java
@@ -39,4 +39,11 @@ public class PersonController {
         return personService.findById(id);
     }
 
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void  deletById(@PathVariable Long id) throws PersonNotFoundExcepetion{
+        System.out.println(id);
+        personService.deletById(id);
+
+    }
 }
